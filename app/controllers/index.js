acdc34c4f65879946ae781dc24e3ff4a4d6f6028
drd-2rcl <drd.rclrd@gmail.com>
2018-06-29 16:51:48 -0300

    auth users pendente

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,23 @@
 module.exports.index = function(application, req, res){
-  res.render('index');
+  res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res){
+  var dadosForm = req.body;
+  req.assert('usuario', 'Usuario não pode ficar vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ficar vazia').notEmpty();
+
+  var erros = req.validationErrors();
+
+  if(erros){
+    res.render("index", {validacao:erros});
+    return;
+  }
+
+  var connection = application.config.dbConnection;
+  var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+  UsuariosDAO.autenticar(dadosForm);
+
+  res.send('tudo ok para criar a sessao');
 }
--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -14,6 +14,18 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario, res){
   this._connection(dados);
 }
 
+// UsuariosDAO.prototype.autenticar = function(usuario){
+//   var dados = {
+//     operacao: "pesquisar",
+//     usuario: usuario,
+//     collection: "usuarios",
+//     callback: function(err, result){
+//       res.send("Cadastro realizado");
+//     }
+//   }
+//   this._connection(dados);
+// }
+
 module.exports = function(){
   return UsuariosDAO;
 }
