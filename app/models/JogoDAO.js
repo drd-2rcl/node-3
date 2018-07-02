8c6fc57dac823d7e9b901742eb2fafcfcdd567b2
drd-2rcl <drd.rclrd@gmail.com>
2018-07-02 15:45:21 -0300

    users

---
+++ app/models/JogoDAO.js
@@ -0,0 +1,19 @@
+// function JogoDAO(connection){
+//   this._connection = connection();
+// }
+//
+// JogoDAO.prototype.gerarParametros = function(){
+//   var dados = {
+//     operacao: "inserir",
+//     usuario: usuario,
+//     collection: "jogo",
+//     callback: function(err, result){
+//       res.send({"Cadastro realizado"});
+//     }
+//   }
+//   this._connection(dados);
+// }
+//
+// module.exports = function(){
+//   return JogoDAO;
+// }
