c02fd4699090cc2368b13c3fc86bdc7ae01fdeb4
drd-2rcl <drd.rclrd@gmail.com>
2018-07-03 11:37:00 -0300

    ajax ok

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -4,6 +4,15 @@ module.exports.jogo = function(application, req, res){
   } else {
     res.send('Usuário precisa fazer login');
   }
+
+  var comando_invalido = 'N';
+  if(req.query.comando_invalido == 'S'){
+      comando_invalido = 'S';
+  }
+
+  console.log(comando_invalido);
+
+
 }
 
 module.exports.sair = function(application, req, res){
@@ -11,4 +20,41 @@ module.exports.sair = function(application, req, res){
   req.session.destroy(function(err){
     res.render("index", {validacao: {}});
   });
+}
+
+module.exports.suditos = function(application, req, res){
+  if(req.session.autorizado !== true){
+    res.send('Usuário precisa fazer login');
+    return;
+  }
+  res.render("aldeoes", {validacao: {}});
+}
+
+module.exports.pergaminhos = function(application, req, res){
+  if(req.session.autorizado !== true){
+    res.send('Usuário precisa fazer login');
+    return;
+  }
+  res.render("pergaminhos", {validacao: {}});
+}
+
+module.exports.ordenar_acao_sudito = function(application, req, res){
+  if(req.session.autorizado !== true){
+    res.send('Usuário precisa fazer login');
+    return;
+  }
+
+  var dadosForm = req.body;
+
+  req.assert('acao', 'Ação deve ser informada').notEmpty();
+  req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+  var erros = req.validationErrors();
+
+  if(erros){
+    res.redirect('jogo?comando_invalido=S');
+    return;
+  }
+  console.log(dadosForm);
+  res.send('tudo ok!');
 }
